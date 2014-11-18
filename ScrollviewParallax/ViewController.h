fe84abe426cdc1cfea1a36873f1be09af92dff26
Valere Fedronic <vfedronic@Valeres-MacBook-Pro.local>
2014-11-18 22:58:25 +0100

    Initial Commit

---
+++ ScrollviewParallax/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScrollviewParallax
+//
+//  Created by Valere Fedronic on 18/11/14.
+//  Copyright (c) 2014 share. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ScrollviewParallax/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ScrollviewParallax
+//
+//  Created by Valere Fedronic on 18/11/14.
+//  Copyright (c) 2014 share. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
